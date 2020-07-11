27104dbf8d67e471b9973ae24ce597ed2b77e8e5
ClausClaus <weixinla84@gmail.com>
2020-07-11 17:18:42 +0800

    添加withCredentials将跨域请求的时的cookie一起携带

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -5,18 +5,24 @@ import qs from 'qs'
 //
 // import NProgress from 'nprogress'
 
-// document.cookie = 'a=b'
-//
-// axios.get('/more/get').then(res => {
-//   console.log(res)
-// })
-//
-// axios.post('http://127.0.0.1:8088/more/server2', {}, {
-//   withCredentials: true
-// }).then(res => {
-//   console.log(res)
-// // })
-//
+document.cookie = 'a=b'
+
+axios.get('/more/get').then(res => {
+  console.log(res)
+})
+
+axios
+  .post(
+    'http://127.0.0.1:8088/more/server2',
+    {},
+    {
+      withCredentials: true
+    }
+  )
+  .then(res => {
+    console.log(res)
+  })
+
 // // const instance = axios.create({
 // //   xsrfCookieName: 'XSRF-TOKEN-D',
 // //   xsrfHeaderName: 'X-XSRF-TOKEN-D'
@@ -94,7 +100,6 @@ import qs from 'qs'
 //   console.log(res)
 // })
 
-
 // axios.get('/more/304').then(res => {
 //   console.log(res)
 // }).catch((e: AxiosError) => {
@@ -151,33 +156,33 @@ import qs from 'qs'
 //
 // instance.get('https://img.mukewang.com/szimg/5becd5ad0001b89306000338-360-202.jpg')
 
-function getA() {
-  return axios.get('/more/A')
-}
+// function getA() {
+//   return axios.get('/more/A')
+// }
 
-function getB() {
-  return axios.get('/more/B')
-}
+// function getB() {
+//   return axios.get('/more/B')
+// }
 
-axios.all([getA(), getB()])
-  .then(axios.spread(function(resA, resB) {
-    console.log(resA.data)
-    console.log(resB.data)
-  }))
+// axios.all([getA(), getB()])
+//   .then(axios.spread(function(resA, resB) {
+//     console.log(resA.data)
+//     console.log(resB.data)
+//   }))
 
-axios.all([getA(), getB()])
-  .then(([resA, resB]) => {
-    console.log(resA.data)
-    console.log(resB.data)
-  })
+// axios.all([getA(), getB()])
+//   .then(([resA, resB]) => {
+//     console.log(resA.data)
+//     console.log(resB.data)
+//   })
 
-const fakeConfig = {
-  baseURL: 'https://www.baidu.com/',
-  url: '/user/12345',
-  params: {
-    idClient: 1,
-    idTest: 2,
-    testString: 'thisIsATest'
-  }
-}
-console.log(axios.getUri(fakeConfig))
+// const fakeConfig = {
+//   baseURL: 'https://www.baidu.com/',
+//   url: '/user/12345',
+//   params: {
+//     idClient: 1,
+//     idTest: 2,
+//     testString: 'thisIsATest'
+//   }
+// }
+// console.log(axios.getUri(fakeConfig))
--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -8,7 +8,16 @@ import { createError } from '../helpers/error'
  */
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
-    const { data = null, url, method = 'get', headers, responseType, timeout, cancelToken } = config
+    const {
+      data = null,
+      url,
+      method = 'get',
+      headers,
+      responseType,
+      timeout,
+      cancelToken,
+      withCredentials
+    } = config
 
     const request = new XMLHttpRequest()
 
@@ -18,6 +27,9 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 
     if (timeout) {
       request.timeout = timeout
+    }
+    if (withCredentials) {
+      request.withCredentials = withCredentials
     }
 
     request.open(method.toUpperCase(), url!, true)
